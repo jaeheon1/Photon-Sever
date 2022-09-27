c6e4a343ae5cf376369cc693ea4345ad22cfdd29
jaeheon1 <jungjaehen@naver.com>
2022-09-27 19:45:29 +0900

    photon map

---
+++ Photon Server/Assets/Script/PhotonControl.cs
@@ -0,0 +1,46 @@
+using Photon.Pun;
+using UnityEngine;
+public class PhotonControl : MonoBehaviourPun
+{
+    [SerializeField] float speed = 5.0f;
+    [SerializeField] float angleSpeed;
+
+    [SerializeField] Camera cam;
+
+    //isMine : 나 자신만 플레이 하고 싶을때
+    void Start()
+    {
+        if (photonView.IsMine)
+        {
+            Camera.main.gameObject.SetActive(false);
+        }
+        else
+        {
+            cam.enabled = false;
+            GetComponentInChildren<AudioListener>().enabled = false;
+        }
+    }
+
+
+    void Update()
+    {
+        //현재 플레이어가 나 자신이 아니라면 
+        if (!photonView.IsMine)
+        {
+            return;
+        }
+
+        Vector3 dir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        transform.Translate(dir.normalized * speed * Time.deltaTime);
+        transform.eulerAngles += new Vector3
+            (
+
+            0,
+            Input.GetAxis("Mouse X") * angleSpeed * Time.deltaTime,
+            0
+
+            );
+
+    }
+}
